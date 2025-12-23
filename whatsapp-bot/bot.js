9b40a3697ad34bb67531ff0bdd01a326f35684be
WahyuSapurata <wm337708@gmail.com>
2025-12-23 17:46:18 +0800

    update boot

--- whatsapp-bot/bot.js
+++ whatsapp-bot/bot.js
@@ -10,20 +10,17 @@ let userOutletMap = new Map();
 
 const client = new Client({
     authStrategy: new LocalAuth({
-        clientId: 'MotoCore-bot'
+        clientId: "MotoCore-bot"
     }),
     puppeteer: {
-        headless: 'new', // ⬅️ penting
-        executablePath: '/usr/bin/google-chrome',
+        headless: 'new',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
             '--disable-gpu',
             '--disable-software-rasterizer',
-            '--no-zygote',
-            '--single-process'
-        ]
+        ],
     }
 });
 
