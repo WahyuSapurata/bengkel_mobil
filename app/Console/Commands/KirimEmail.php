63fd62d3982d9dde8b2dd844c001a4c3e5304766
WahyuSapurata <wm337708@gmail.com>
2026-02-05 10:14:56 +0800

    update data KirimEmail and laporan transaksi

--- app/Console/Commands/KirimEmail.php
+++ app/Console/Commands/KirimEmail.php
@@ -29,8 +29,8 @@ class KirimEmail extends Command
      */
     public function handle()
     {
-        // $tanggalHariIni = Carbon::now()->format('d-m-Y');
-        $tanggalHariIni = "01-12-2026";
+        $tanggalHariIni = Carbon::now()->format('d-m-Y');
+        // $tanggalHariIni = "01-12-2026";
         // $emailTujuan = 'wm337708@gmail.com';
         $emailTujuan = 'mmmaeroautomotive@gmail.com';
 
